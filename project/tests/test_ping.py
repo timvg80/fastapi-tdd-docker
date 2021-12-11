b5057edca6499085af1c1725254c92489baf91d0
Tim Vivian-Griffiths <tim.vivian.griffiths@tutanota.com>
2021-12-11 19:30:33 +0000

    completing pytest chapter

---
+++ project/tests/test_ping.py
@@ -0,0 +1,16 @@
+# project/tests/test_ping.py
+
+
+from app import main
+
+
+def test_ping(test_app):
+    # given
+    # test_app
+
+    # when
+    response = test_app.get("/ping")
+
+    # then
+    assert response.status_code == 200
+    assert response.json() == {"environment": "dev", "ping": "pong!", "testing": True}
